58f1940d010cbf6ca3a25f358da94e8fc962b522
Jonghwan Hyeon <hyeon0145@gmail.com>
2020-12-14 22:57:32 +0900

    Add message parameter

--- profile/profile.py
+++ profile/profile.py
@@ -1,18 +1,27 @@
+import sys
 import time
+
 from contextlib import contextmanager
 
 
 @contextmanager
-def profile():
+def profile(message=None):
     start_time, end_time = time.time(), 0
     yield lambda: end_time - start_time
     end_time = time.time()
 
+    if message is not None:
+        print(message.format(end_time - start_time), file=sys.stderr)
+
 
 if __name__ == '__main__':
+    with profile('Elapsed time: {:.2f}s'):
+        for _ in range(100):
+            time.sleep(1 / 100)
+    # Elapsed time: 1.09s
+
     with profile() as elapsed:
         for _ in range(100):
             time.sleep(1 / 100)
     print(f'Elapsed time: {elapsed():.2f}s')
     # Elapsed time: 1.09s
-
